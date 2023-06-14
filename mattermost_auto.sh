07d59f887b0ee30654671ff5cc5f4874bff7b092
NonneTrapuE <51425313+NonneTrapuE@users.noreply.github.com>
2023-06-14 20:01:22 +0200

    Add files via upload
    Correction des erreurs de syntaxe
    Correction des problèmes de droits

--- mattermost_auto.sh
+++ mattermost_auto.sh
@@ -33,9 +33,9 @@ touch $LOG_PATH
 
 clear
 echo -e "\n" >> $LOG_PATH
-echo "======================================================" | tee $LOG_PATH
-echo "Installation de PostgreSQL" | tee $LOG_PATH
-echo "##########################" | tee $LOG_PATH
+echo "==========================" | tee -a $LOG_PATH
+echo "Installation de PostgreSQL" | tee -a $LOG_PATH
+echo "##########################" | tee -a $LOG_PATH
 
 	#Timestamp
 echo date | cut --delimiter=" " -f 2,3,4 >> $LOG_PATH
@@ -44,11 +44,11 @@ echo date | cut --delimiter=" " -f 2,3,4 >> $LOG_PATH
 dnf install -y postgresql-server postgresql-contrib
 POSTGRE_INSTALL_ERROR_EXIT_CODE=$?
 
-if [ $POSTGRE_INSTALL_ERROR_MESSAGE = 0 ]; then
-	echo "Installation de PostgreSQL: OK" | tee $LOG_PATH
+if [ "$POSTGRE_INSTALL_ERROR_EXIT_CODE" = "0" ]; then
+	echo "Installation de PostgreSQL: OK" | tee -a $LOG_PATH
 else
-	echo "Erreur d'installation." | tee $LOG_PATH
-	echo $POSTGRE_INSTALL_ERROR_MESSAGE >> $LOG_PATH
+	echo "Erreur d'installation." | tee -a $LOG_PATH
+	echo $POSTGRE_INSTALL_ERROR_EXIT_CODE >> $LOG_PATH
 	exit 1
 fi
 
@@ -56,7 +56,7 @@ fi
 
 POSTGRES_USER=$(cat /etc/passwd | grep postgres | cut --delimiter=":" -f 1)
 
-if [ $POSTGRES_USER -eq "postgres" ]; then
+if [ "$POSTGRES_USER" = "postgres" ]; then
 	echo "L'utilisateur $POSTGRES_USER est existant" 
 else 
 	echo "L'utilisateur $POSTGRES_USER n'existe pas."
@@ -64,11 +64,11 @@ fi
 
 	#Connexion à l'utilisateur postgres/initialisation de la base de données
 
-echo "Initialisation de PostgreSQL" | tee $LOG_PATH
+echo "Initialisation de PostgreSQL" | tee -a $LOG_PATH
 postgresql-setup --initdb --unit postgresql
-$POSTGRE_SETUP_EXIT_CODE=$? 
+POSTGRE_SETUP_EXIT_CODE=$? 
 
-if [ $POSTGRE_SETUP_EXIT_CODE = 0 ]; then
+if [ "$POSTGRE_SETUP_EXIT_CODE" = "0" ]; then
 	systemctl enable --now postgresql 2>> $LOG_PATH
 else
 	exit 2
@@ -82,18 +82,18 @@ echo -e "Opérations sur la base de données \n #######################"
 read -p "Entrez le nom de la base de données: " DATABASE_NAME
 read -p "Entrez l'utilisateur de la base de données: " DATABASE_USER
 read -s -p "Entrez un mot de passe pour l'utilisateur: " DATABASE_USER_PASSWORD
-sudo -u postgres -- psql --command="CREATE DATABASE $DATABASE_NAME WITH ENCODING 'UTF8' LC_COLLATE='fr_FR.UT-8' LC_CTYPE='fr_FR.UTF8' TEMPLATE=template0;"
-sudo -u postgres -- psql --command="CREATE USER $DATABASE_USER WITH PASSWORD $DATABASE_USER_PASSWORD;"
-sudo -u postgres -- psql --command="GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NAME to $DATABASE_USER;"
-sudo -u postgres -- psql --command="ALTER DATABASE $DATABASE_NAME OWNER TO $DATABASE_USER;"
-sudo -u postgres -- psql --command="GRANT USAGE, CREATE ON SCHEMA PUBLIC TO $DATABASE_USER;"
+sudo -E -u postgres psql --command="CREATE DATABASE $DATABASE_NAME WITH ENCODING 'UTF8' LC_COLLATE='fr_FR.UTF-8' LC_CTYPE='fr_FR.UTF8' TEMPLATE=template0;"
+sudo -E -u postgres psql --command="CREATE USER $DATABASE_USER WITH PASSWORD '$DATABASE_USER_PASSWORD';"
+sudo -E -u postgres psql --command="GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NAME to $DATABASE_USER;"
+sudo -E -u postgres psql --command="ALTER DATABASE $DATABASE_NAME OWNER TO $DATABASE_USER;"
+sudo -E -u postgres psql --command="GRANT USAGE, CREATE ON SCHEMA PUBLIC TO $DATABASE_USER;"
 
 
 	#Changement du propriétaire du répertoire/var/lib/pgsql
 
 PG_CONF_FILE=/var/lib/pgsql/data/pg_hba.conf
-chown -vR $USERNAME /var/lib/pgsql | tee $LOG_PATH
-mv -vf $PG_CONF_FILE $PG_CONF_FILE.bak | tee $LOG_PATH
+chown -vR $USERNAME /var/lib/pgsql | tee -a $LOG_PATH
+mv -vf $PG_CONF_FILE $PG_CONF_FILE.bak | tee -a $LOG_PATH
 
 	#Approvisionnement du fichier .conf
 echo "Approvisionnement du fichier $PG_CONF_FILE"
@@ -111,10 +111,11 @@ echo "host    replication     all             ::1/128                 ident" >> 
 chown -vR postgres /var/lib/pgsql
 
 	#Redémarrage du service postgresql
+	
 echo "Redémarrage du service postgreSQL"
 systemctl restart postgresql.service 2>> $LOG_PATH
 
-if [$? = 0]; then
+if [ "$?" = "0" ]; then
 	echo "Redémarrage du service : OK"
 	exit 0
 else
